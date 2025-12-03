592200802f24ff4fd3d7e589dbf9a15e3649bc3d
sharky564 <sharkesa@gmail.com>
2025-12-03 16:34:10 +1100

    Day 3

--- q03.py
+++ q03.py
@@ -1,17 +1,38 @@
 from utils import run_solver
 
 def process_data(d: list[str]):
-    return d
+    return [list(map(int, line)) for line in d]
+
+
+def find_largest_subsequence(b, l):
+    v = 0
+    while l > 0:
+        l -= 1
+        if l > 0:
+            s = max(b[:-l])
+        else:
+            s = max(b)
+        b = b[b.index(s) + 1:]
+        v = 10 * v + s
+    return v
+
 
 @run_solver("Part 1", submit_result=False)
 def part1(d: list[str]) -> int:
     data = process_data(d)
-    return None
+    t = 0
+    for b in data:
+        t += find_largest_subsequence(b, 2)
+    return t
 
 @run_solver("Part 2", submit_result=False)
 def part2(d: list[str]) -> int:
     data = process_data(d)
-    return None
+    t = 0
+    for b in data:
+        x = find_largest_subsequence(b, 12)
+        t += x
+    return t
 
 if __name__ == '__main__':
     part1()
